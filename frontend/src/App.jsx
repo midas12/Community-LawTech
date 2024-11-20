33ebf4fb7f804c29436946a96cc42c65a2dc03bc
G123by <117691959+G123by@users.noreply.github.com>
2024-11-20 16:10:14 +0000

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,32 @@ function App() {
     <div>
       {/* <LawyerSignupForm />
       <LawyerOnboardingForm /> */}
-      <Header />
+      <header className="header-box">
+<div className="left-text">Law Tech</div>
+      <nav className="nav">
+        <a href="#home">Home</a>
+        <a href="#about">About Us</a>
+        <a href="#services">Services</a>
+        <a href="#contact">Donate</a>
+      </nav>
+      <div className="header-buttons">
+        <button className="btn search-btn">Search Lawyers</button>
+        <button className="btn get-involved-btn">Get Involved</button>
+      </div>
+    </header>
+
+
+      
+    <footer className="footer-box">
+        <hr className="footer-line" />
+        <div className="footer-text">
+          <p className="copyright-text">
+            © {new Date().getFullYear()} Law Tech. All rights reserved.
+          </p>
+          <a href="#terms" className="terms-link">Terms of Service</a>
+          <a href="#terms" className="privacy-link">Privacy Policy</a>
+        </div>
+      </footer>
 
     </div>
   );
