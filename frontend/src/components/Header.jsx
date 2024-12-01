8c6fbd4d25c0580010aa1476ee66b8fadb9d72af
G123by <117691959+G123by@users.noreply.github.com>
2024-12-01 04:23:32 +0000

    Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -34,7 +34,7 @@ function Header({ setShowForm }) {
           <button className="btn btn-outline-light me-2" onClick={() => navigate("/lawyer-registration")}>
             Get Involved
           </button>
-          <button className="btn btn-login" onClick={() => navigate("/login-page")}>
+          <button className="btn btn-login" onClick={() => navigate("/login")}>
             Login
           </button>
         </div>
