f7d52ee6041a818fa5ec0dc7b546fae0ea6472c5
G123by <117691959+G123by@users.noreply.github.com>
2024-12-04 05:16:59 +0000

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Mission from "./components/Mission";
 import OurLegalSupport from "./components/OurLegalSupport";
 import AboutUs from "./components/AboutUs";
 import FindLawyer from "./components/FindLawyer";
+import ServicePage from "./components/ServicePage";
 import SupportOurMission from "./components/SupportOurMission";
 
 // Formz
@@ -18,6 +19,7 @@ import Login from "./components/Login";
 import AdminDashboard from "./components/AdminDashboard";
 import AdminManageLawyers from "./components/AdminManageLawyers";
 import AdminLoginForm from "./components/AdminLoginForm";
+import LawyerHomePage from "./components/LawyerHomePage";
 
 const App = () => {
 
@@ -34,12 +36,14 @@ const App = () => {
           <Route path="/mission" element={<Mission />} />
           <Route path="/our-legal-support" element={<OurLegalSupport />} />
           <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/service" element={<ServicePage />} />
           <Route path="/find-lawyer" element={<FindLawyer />} />
           <Route path="/support-our-mission" element={<SupportOurMission />} />
           <Route path="/lawyer-onboarding" element={<LawyerOnboardingForm />} />
           <Route path="/lawyer-registration" element={<LawyerRegistrationForm />} />
           <Route path="/user-preferences" element={<UserPreferencesForm />} />
           <Route path="/login" element={<Login/>} />
+          <Route path="/lawyer-home" element={<LawyerHomePage />} />
         </Routes>
       </main>
       <Footer />
