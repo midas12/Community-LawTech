7aad03e31ac84a71c06f2db65a9f783526f82ee2
G123by <117691959+G123by@users.noreply.github.com>
2024-12-20 10:11:29 +0000

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ import EmploymentServicePage from "./components/EmploymentServicePage";
 import HousingSupportPage from "./components/HousingSupportPage";
 import DonatePage from "./components/DonatePage";
 import GoogleTranslate from "./components/GoogleTranslate";
-import ClientFeedackForm from "./components/ClientFeedackForm";
+// import ClientFeedackForm from "./components/ClientFeedackForm";
 
 const App = () => {
 
@@ -58,7 +58,7 @@ const App = () => {
           <Route path="/employment" element={<EmploymentServicePage/>} />
           <Route path="/donate" element={<DonatePage />} />
           <Route path="/google-translate" element={<GoogleTranslate />} />
-          <Route path="/client-feedback" element={<ClientFeedackForm />} />
+{/*           <Route path="/client-feedback" element={<ClientFeedackForm />} /> */}
         </Routes>
       </main>
       <Footer />
